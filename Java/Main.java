ed96355e43d75970a060396432383edf4f13f7e0
zizaglez <zizagonzalez@gmail.com>
2021-10-19 20:22:27 -0500

    My firs class of java class, called human one attribute and 3 methods

---
+++ Java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Main class
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Learning Object Oriented Programming with Java");
+        
+    }
+}
---
+++ Java/classes/Human.java
@@ -0,0 +1,21 @@
+public class Human{
+
+        //Attributes / Properties. en java los atributos y los metodos hay que definir el tipo
+        int born_year = 1987;
+
+        public void say_hello(){ //Estos son mis metodos
+            System.out.println(" Hello ");
+
+        }
+
+        public String Say_hello_user(String user_name){
+            return "Hello " + user_name;
+
+        }
+
+        public int born_year(int year){
+            return year;
+
+        }
+        
+    }
