7b1bd5623fb8c1586b8ffa943006b52ac6a07512
zizaglez <zizagonzalez@gmail.com>
2021-10-19 19:28:02 -0500

    Second commit, loading human an exam practise

--- Carros.py
+++
@@ -1,9 +0,0 @@
-class Carro:
-
-    color = "rojo"
-
-    def color(self):
-        print("Color")
-
-carro_one = Carro()
-print(carro_one)
--- Human.py
+++ Human.py
@@ -15,3 +15,10 @@ print(human_one)
 print(human_one.born_year)
 human_one.say_hello()#aqui estamos invocando
 human_one.say_hello_user("Cesar")#aqui estamos invocando
+
+
+#Abstracion: Cuando separamos los datos de un objeto para luego generar un molde o una clase.
+#Encapsulamiento: Para hacer que ciertas metodos o propiedades sean inviolabes o inalterables
+#Herencia: Permite crear clases apartir de otras. Si tuvieramos una clase "autos" y quisieramos crear una clase de "Auto deportivo" o "Auto clasico" podriamos
+#propiedades y metodos de la clase " autos". Esto nos da una jerarquia de padre e hijo
+# Se utiliza para crear "metodos" con el mismo nombre pero con diferente comportamiento
