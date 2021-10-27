a5be273e5f5349bede4187fdaf9da714e58895ea
zizaglez <zizagonzalez@gmail.com>
2021-10-27 17:50:34 -0500

    Trabajo en clase resulto

--- Java/Main.java
+++ Java/Main.java
@@ -2,11 +2,13 @@ import classes.User;
 
 public class Main {
 
-    public static int main(String[] args) {
+    public static void main(String[] args) {
 
-        //System.out.println("Learning Object Oriented Programming with Java");
-        Object object_user = new User(0122110655, " Cesar G", "0122110655@alumnos.univa.com.mx");
-        System.out.println(object_user.id);
+        System.out.println("Learning Object Oriented Programming with Java");
+        User object_user = new User(0122110655, " Cesar G", "0122110655@alumnos.univa.com.mx");
+        System.out.println("User id: " + object_user.id);
+        System.out.println("Full name: " + object_user.full_name);
+        System.out.println("Personal Email: " + object_user.personal_email);
 
         
     }
--- Java/classes/User.java
+++ Java/classes/User.java
@@ -1,12 +1,13 @@
 package classes;
 
 public class User{
-    int id;
-    String full_name;
-    String personal_email;
+    public int id;
+    public String full_name;
+    public String personal_email;
 
     //This is the constructor in java
     public User(int id, String full_name, String personal_email){
+        super();
         this.id = id;
         this.full_name = full_name;
         this.personal_email = personal_email;
