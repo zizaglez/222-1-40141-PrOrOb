264a7aaddeeb1eac4bcdcf847a8c77dd3cd18e23
zizaglez <zizagonzalez@gmail.com>
2021-11-03 22:36:29 -0600

    examen

--- Python/course.py
+++ Python/course.py
@@ -1,28 +1,50 @@
-from _typeshed import Self
-
-
 class Course:
-
-    nombre = "POO"
-    profesor = "Luis Guerra"
-    horario = 7
-    turno = "Impulso"
-    salon: 5202
-    alumnos: 7
-
-    def __init__(self, clase, profesor, horario, turno, salon, alumnos):#(def __init__ es mi contructor)Un CONSTRUCTOR es Obligarte a inicializar los atributos de un objeto
-        self.clase = clase
-        self.profesor = profesor
-        self.horario = horario
+    materia = "POO"
+    cuatrimestre = 4
+    def __init__(self, materia, turno, cuatrimestre, profesor):
+        self.materia = materia
         self.turno = turno
-        self.salon = 5202
-        self.alumnos = 7        
+        self.cuatrimestre = cuatrimestre
+        self.profesor = profesor
+      
+mi_primer_curso = Course("POO", "turno impulso", 4, "Luis Guerra")
+mi_segundo_curso = Course("programacion multimedia", "modelo impulso", 4, "Sinai Bucio")
 
-object_course = Course("La materia de POO", " es dada por el profesor Luis Guerra", "a las 7:00pm", " en el turno matutino")
-#print(object_user)
-print(str(object_course.clase))
-print(object_course.profesor)
-print(object_course.horario)
-print(object_course.turno)
+def cursos_univa(mi_primer_curso, mi_segundo_curso):
+    print("Mis cursos son: " + mi_primer_curso.materia + " y " + mi_segundo_curso.materia)
+
+def maestros_univa(mi_primer_curso, mi_segundo_curso):
+    print("Mis cursos son: " + mi_primer_curso.profesor + " y " + mi_segundo_curso.profesor)
+
+cursos_univa(mi_primer_curso,mi_segundo_curso)
+maestros_univa(mi_primer_curso,mi_segundo_curso)
+
+def materia():
+    print("Mi materia actual: " + Course.materia)
+
+def cuatrimestre():
+    print("Mis dos materias son de " + str(Course.cuatrimestre) + "to cuatrimestre.")
+
+materia()
+cuatrimestre()
+
+
+import Classes.Course;
+
+public class Main2{
+
+    public static void main(String[] args){
+
+        //System.out.println("Learning Object Oriented Programming with Java");
+        Course course_in_progress = new Course("POO ","Luis Guerra a las ", "7 pm ", " somos el grupo 204");
+        Course my_next_course = new Course("Programación multimedia", "Sinai Bucio ", "8" ," grupo 256");
+
+        System.out.println(course_in_progress.nombre_profesor(course_in_progress.materia, course_in_progress.profesor ,course_in_progress.grupo));
+        System.out.println("Mi siguiente materia es "+ my_next_course.materia + " la imparte " + my_next_course.profesor +" los jueves a las " + my_next_course.grupo);
+
+        Course.hola_amigos();
+        course_in_progress.course_class();
+    }
+}
 
 
