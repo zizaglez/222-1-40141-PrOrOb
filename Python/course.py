540ec6f5710f1c146593cc4a17ce1586e74c7e85
zizaglez <zizagonzalez@gmail.com>
2021-11-03 22:37:39 -0600

    examen

--- Python/course.py
+++ Python/course.py
@@ -29,22 +29,3 @@ materia()
 cuatrimestre()
 
 
-import Classes.Course;
-
-public class Main2{
-
-    public static void main(String[] args){
-
-        //System.out.println("Learning Object Oriented Programming with Java");
-        Course course_in_progress = new Course("POO ","Luis Guerra a las ", "7 pm ", " somos el grupo 204");
-        Course my_next_course = new Course("Programación multimedia", "Sinai Bucio ", "8" ," grupo 256");
-
-        System.out.println(course_in_progress.nombre_profesor(course_in_progress.materia, course_in_progress.profesor ,course_in_progress.grupo));
-        System.out.println("Mi siguiente materia es "+ my_next_course.materia + " la imparte " + my_next_course.profesor +" los jueves a las " + my_next_course.grupo);
-
-        Course.hola_amigos();
-        course_in_progress.course_class();
-    }
-}
-
-
